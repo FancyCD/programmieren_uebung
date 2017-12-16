fdd7a9862fcc3f8da78dd16241dcfb552f96f93b
Chijiang <chijiang93@gmail.com>
2017-12-16 21:57:46 +0100

    Bomber bug fixed

--- Bomber.java
+++ Bomber.java
@@ -11,22 +11,28 @@ class Bomber extends Player {
 	* @return true, falls die Zielposition effekiv ist.
 	*/
 	public boolean shoot(int[] pos, Map map) {
+		boolean hasAlien = false;
 		if (pos[0] >= map.getMap().length || pos[1] >= map.getMap()[0].length) {
 			System.out.println("Die Zielposition ist ausserhalb der Grenze.");
-			return false;
+			return hasAlien;
 		}
 		for (Alien al : map.getAliens()) {
 			if ((pos[0] - 2 < al.getPos()[0] && al.getPos()[0] < pos[0] + 2) 
 				&& (pos[1] - 2 < al.getPos()[1] && al.getPos()[1] < pos[1] + 2)) {
+				hasAlien = true;
 				double t =  Math.random() * 4 / al.distance(getPos());
 				if (t > 0.5 && al.isAlive()) {
 					System.out.printf("Der Bomber hat das Alien (%d, %d) getroffen!\n", al.getPos()[0], al.getPos()[1]);
 					al.tot();
-					return true;
+				} else {
+					System.out.printf("Der Bomber hat das Alien (%d, %d) verfelt!\n", al.getPos()[0], al.getPos()[1]);
 				}
+
 			}
 		}
-		System.out.println("Kein Alien auf der Zielbereich.");
-		return false;
+		if (!hasAlien) {
+			System.out.println("Kein Alien auf der Zielbereich.");
+		}
+		return hasAlien;
 	}
 }
